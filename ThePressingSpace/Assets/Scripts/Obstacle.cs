c27e7cb62ecd7b74d90e4132058e28643158f72d
Vitória <vitoriamaar@gmai.com>
2019-04-27 14:56:38 -0300

    Initial game
    Gamezinho ae

---
+++ ThePressingSpace/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    public int damage = 1;
+    public float speed;
+ 
+    void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+
+    }
+}
---
+++ ThePressingSpace/Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 0;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Vector2 force = transform.up * speed;
+            rb.AddForce(force, ForceMode2D.Force);
+        }
+    }
+}
